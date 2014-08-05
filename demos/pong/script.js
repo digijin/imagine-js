6a26c9dc53cad6852776f7a159cddc176828392c
James Smith <james@digijin.com>
2014-08-05 15:15:47 +1000

    pong demo

--- demos/pong/script.js
+++ demos/pong/script.js
@@ -1,6 +1,38 @@
 var player;
 $(document).ready(function(){
 
+	collider = function(){
+		return {
+			name:'collider',
+			start: function(){
+				this.element = $(this.getComponent('element'));
+			},
+			collidesWith: function(obj){
+				obj = $(obj);
+				var top = parseFloat(this.element.css('top'));
+				var left = parseFloat(this.element.css('left'));
+				var width = this.element.width();
+				var height = this.element.height();
+				var bottom = top+height;
+				var right = left+width;
+				var o_top = parseFloat(obj.css('top'));
+				var o_left = parseFloat(obj.css('left'));
+				var o_width = obj.width();
+				var o_height = obj.height();
+				var o_bottom = o_top+o_height;
+				var o_right = o_left+o_width;
+
+				var outsideH = bottom < o_top ||
+					o_bottom < top ;
+				var outsideV = right < o_left ||
+					o_right < left;
+
+				return !outsideV && !outsideH;
+
+			}
+		};
+	};
+
 	paddle = function(){
 		return {
 			name: 'paddle',
@@ -18,18 +50,16 @@ $(document).ready(function(){
 				if(parseFloat(this.element.css('top'))<=0){
 					this.element.css('top', 0);					
 				}
-
-				
 				if(parseFloat(this.element.css('bottom'))<=0){
-
 					this.element.css('top', this.element.parent().height() - this.element.height() - 1);
 				}
 				
 			}
-		}
+		};
 	};
 
 	playerComponent = {
+		name: 'player',
 		start: function(){
 			this.paddle = this.getComponent('paddle');
 		},
@@ -44,6 +74,7 @@ $(document).ready(function(){
 	};
 
 	enemyComponent = {
+		name: 'enemy',
 		start: function(){
 			this.paddle = this.getComponent('paddle');
 			this.ball = $(Imagine.getComponent('ball').getComponent('element'));
@@ -70,6 +101,8 @@ $(document).ready(function(){
 			var left = parseFloat(this.element.css('left'));
 			var top = parseFloat(this.element.css('top'));
 
+			
+
 			if(left + this.element.width() >= this.element.parent().width()-5){
 				if(this.dirH>0)
 					this.dirH = -this.dirH;
@@ -86,13 +119,22 @@ $(document).ready(function(){
 					this.dirV = -this.dirV;
 			} //HIT TOP OR BOTTOM
 
+			if(Imagine.getComponent('player').getComponent('collider').collidesWith(this.element)){
+				this.dirH = 1;
+			}
+			if(Imagine.getComponent('enemy').getComponent('collider').collidesWith(this.element)){
+				this.dirH = -1;
+			}
+
 			this.element.css('left', left+(dt*this.dirH*speed));
 			this.element.css('top' , top +(dt*this.dirV*speed));
+
+
 		}
 	};
 
 
 	ball = Imagine($('#ball')[0]).addComponent(ballComponent);
-	player = Imagine($('#left')[0]).addComponent(paddle()).addComponent(playerComponent);
-	enemy = Imagine($('#right')[0]).addComponent(paddle()).addComponent(enemyComponent);
+	player = Imagine($('#left')[0]).addComponent(paddle()).addComponent(collider()).addComponent(playerComponent);
+	enemy = Imagine($('#right')[0]).addComponent(paddle()).addComponent(collider()).addComponent(enemyComponent);
 });
