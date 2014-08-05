2c96a4ca022e8e23ee2efcacb01ca821c54f8e23
James Smith <james@digijin.com>
2014-08-05 14:15:09 +1000

    more work on pong demo

--- demos/pong/script.js
+++ demos/pong/script.js
@@ -1,20 +1,35 @@
 var player;
 $(document).ready(function(){
 
-	paddle = {
-		name: 'paddle',
-		element: $('#left'),
-		dirV: 0,
-		update: function(){
-				
+	paddle = function(){
+		return {
+			name: 'paddle',
+			dirV: 0,
+			start: function(){
+				this.element = $(this.getComponent('element'));
+			},
+			update: function(){
 				var dt = Imagine.Time.deltaTime;
 				var speed = 200; 
 				var top = parseFloat(this.element.css('top'));
 				this.element.css('top' , top +(dt*this.dirV*speed));
+
+				//cap top n bottom
+				if(parseFloat(this.element.css('top'))<=0){
+					this.element.css('top', 0);					
+				}
+
+				
+				if(parseFloat(this.element.css('bottom'))<=0){
+
+					this.element.css('top', this.element.parent().height() - this.element.height() - 1);
+				}
+				
+			}
 		}
 	};
 
-	player = {
+	playerComponent = {
 		start: function(){
 			this.paddle = this.getComponent('paddle');
 		},
@@ -28,11 +43,27 @@ $(document).ready(function(){
 		}
 	};
 
+	enemyComponent = {
+		start: function(){
+			this.paddle = this.getComponent('paddle');
+			this.ball = $(Imagine.getComponent('ball').getComponent('element'));
+			this.element = $(this.getComponent('element'));
+		},
+		update: function(){
+			var balltop = parseFloat(this.ball.css('top'));
+			//console.log(this.element.height());
+			var top = parseFloat(this.element.css('top')) + (this.element.height()/2);
+			this.paddle.dirV = balltop>top?1:-1;
+		}
+	};
 
-	Imagine({
-		element: $('#ball'),
+	ballComponent = {
+		name: 'ball',
 		dirH:1,
 		dirV:1,
+		start: function(){
+			this.element = $(this.getComponent('element'));
+		},
 		update: function(){
 			var speed = 200; 
 			var dt = Imagine.Time.deltaTime;
@@ -58,9 +89,10 @@ $(document).ready(function(){
 			this.element.css('left', left+(dt*this.dirH*speed));
 			this.element.css('top' , top +(dt*this.dirV*speed));
 		}
-	});
+	};
 
 
-	Imagine({}).addComponent(paddle).addComponent(player);
-
+	ball = Imagine($('#ball')[0]).addComponent(ballComponent);
+	player = Imagine($('#left')[0]).addComponent(paddle()).addComponent(playerComponent);
+	enemy = Imagine($('#right')[0]).addComponent(paddle()).addComponent(enemyComponent);
 });
