93d684176e32cc64692aefffdfbf4b836dba0015
James Smith <james@digijin.com>
2014-08-08 01:38:50 +1000

    collider component

--- spec/component/collider.spec.js
+++ spec/component/collider.spec.js
@@ -8,4 +8,39 @@ describe("Imagine.collider", function(){
 		expect(coll.name).toBe('collider');
 		expect(coll.tags.indexOf('collider')).not.toBe(-1);
 	})
+
+	describe("start", function(){
+		it("should set element"
+		// 	, function(){
+		// 	var coll = Imagine.collider();
+		// 	var div = document.createElement("DIV");
+		// 	expect(coll).toBeDefined();
+		// 	com = Imagine({})
+		// 		.addComponent({name:"element"})
+		// 		.addComponent(coll);
+		// 	expect(com).toBeDefined();
+		// 	expect(com.element).toBeDefined();
+		// }
+		)
+	})
+
+	describe("collidesWith", function(){
+		
+	})
+	describe("compareSquares", function(){
+		it("should have basic expectations", function(){
+			var coll = Imagine.collider();
+			expect(coll).toBeDefined();
+			expect(coll.compareSquares).toBeDefined();
+		})
+
+		it("should return false when squares apart", function(){
+			var coll = Imagine.collider();
+			var sq1 = {	t:1,	l:1,	b:2,	r:2};
+			var sq2 = {	t:11,	l:11,	b:12,	r:12};
+			expect(coll.compareSquares(sq1, sq2)).toBe(false);
+		})
+		
+			
+	})
 })
--- src/component/collider.coffee
+++ src/component/collider.coffee
@@ -1,4 +1,37 @@
 Imagine.collider = ->
-	name: 'collider'
-	tags: ['collider']
-	
+  name: 'collider'
+  tags: ['collider']
+  start: ->
+    @element = @getComponent("element")
+    return
+
+  collidesWith: (obj) ->
+    obj = $(obj)
+    top = parseFloat(@element.css("top"))
+    left = parseFloat(@element.css("left"))
+    bottom = top + @element.height()
+    right = left + @element.width()
+    o_top = parseFloat(obj.css("top"))
+    o_left = parseFloat(obj.css("left"))
+    o_bottom = o_top + obj.height()
+    o_right = o_left + obj.width()
+
+    compareSquares
+      t:top
+      r:right
+      b:bottom
+      l:left
+    ,
+      t:o_top
+      r:o_right
+      b:o_bottom
+      l:o_left
+    outsideH = bottom < o_top or o_bottom < top
+    outsideV = right < o_left or o_right < left
+    not outsideV and not outsideH
+
+  compareSquares: (sq1, sq2) ->
+
+    outsideH = sq1.b < sq2.t or sq2.b < sq1.t
+    outsideV = sq1.r < sq2.l or sq2.r < sq1.l
+    not outsideV and not outsideH
