cb74c524010bfd70be8592924998da39a4e94cb2
James Smith <james@digijin.com>
2014-09-13 23:50:54 +1000

    added some random spec

--- spec/etc.spec.js
+++ spec/etc.spec.js
@@ -1,4 +1,13 @@
 describe("etc", function(){
 	it("should be coffeelinted")
 	it("sohuld merge imagien and engine component init thingys")
+
+	it("blah", function(){
+		comp = {
+			name= "whatevs"
+		}
+		obj = Imagine(comp);
+		expect(obj).toBe(comp);
+		expect(obj.name).toBe(comp.name);
+	})
 })
