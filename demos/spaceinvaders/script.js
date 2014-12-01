c1f44b262fef1271b1b35620ee7a1cf268f9b06f
James Smith <james@digijin.com>
2014-12-01 22:35:02 +1100

    cleaned up space invaders

---
+++ demos/spaceinvaders/script.js
@@ -0,0 +1,72 @@
+(function() {
+  $(document).ready(function() {
+    Number.prototype.clamp = function(min, max) {
+      return Math.min(Math.max(this, min), max);
+    };
+    Imagine.engine.setFPS(60);
+    Imagine({
+      el: $("#player"),
+      update: function() {
+        var dt, left, speed;
+        dt = Imagine.Time.deltaTime;
+        speed = 100;
+        left = parseFloat(this.el.css("left"));
+        left += dt * speed * Imagine.Input.getAxis("Horizontal");
+        left = left.clamp(1, this.el.parent().width() - this.el.width() - 3);
+        this.el.css("left", left);
+      }
+    });
+    Imagine({
+      el: $("#enemyGroup"),
+      speed: 100,
+      downSpeed: 10,
+      top: 10,
+      topGap: 10,
+      stage: "r",
+      start: function() {
+        this.el.css("top", this.top);
+      },
+      update: function() {
+        var dt, left, max, top;
+        dt = Imagine.Time.deltaTime;
+        left = parseFloat(this.el.css("left"));
+        top = parseFloat(this.el.css("top"));
+        switch (this.stage) {
+          case "r":
+            left += dt * this.speed;
+            max = this.el.parent().width() - this.el.width();
+            if (left >= max) {
+              left = max;
+              this.stage = "d";
+              this.top += this.topGap;
+            }
+            return this.el.css("left", left);
+          case "d":
+            top += dt * this.downSpeed;
+            if (top > this.top) {
+              top = this.top;
+              if (left > 100) {
+                this.stage = "l";
+              } else {
+                this.stage = "r";
+              }
+            }
+            if (top > 200) {
+              this.top = 0;
+              top = 0;
+            }
+            return this.el.css("top", top);
+          case "l":
+            left -= dt * this.speed;
+            if (left < 0) {
+              left = 0;
+              this.stage = "d";
+              this.top += this.topGap;
+            }
+            return this.el.css("left", left);
+        }
+      }
+    });
+  });
+
+}).call(this);
