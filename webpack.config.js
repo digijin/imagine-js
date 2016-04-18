569be483a927660c237f737cde9941e27f81f452
James Smith <james@digijin.com>
2016-04-18 21:43:13 +1000

    copypasta from webpack-babel-demo worked

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
+/* global __dirname */
+
 var path = require('path');
 
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var dir_js = path.resolve(__dirname, 'src');
+// var dir_html = path.resolve(__dirname, 'html');
+var dir_build = path.resolve(__dirname, 'build');
+
 module.exports = {
-    entry: "./src/imagine.js",
+    entry: path.resolve(dir_js, 'imagine.js'),
     output: {
-        path: path.join(__dirname, 'lib'),
-        filename: "imagine.js"
+        path: dir_build,
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: dir_build,
     },
     module: {
         loaders: [
             {
-              test: '/\.js/',
-              loader: 'babel-loader',
-              query: {
-                presets: ['es2015']
-              }
+                loader: 'babel-loader',
+                test: dir_js,
             }
         ]
-    }
+    },
+    plugins: [
+        // Simply copies the files over
+        // new CopyWebpackPlugin([
+        //     { from: dir_html } // to: output.path
+        // ]),
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
 };
