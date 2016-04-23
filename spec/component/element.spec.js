67d7bc678758ffa6effaa92abf73f2d038340453
James Smith <james@digijin.com>
2016-04-23 20:57:15 +1000

    element

--- spec/component/element.spec.js
+++ spec/component/element.spec.js
@@ -1,5 +1,6 @@
 var Element = require('src/component/element');
 var $ = require('jquery');
+var utils = require('src/imagine/utils');
 
 if(jasmine.getFixtures){
 	jasmine.getFixtures().fixturesPath = 'spec/fixtures';
@@ -31,6 +32,7 @@ describe("Imagine/component/element", function(){
 		expect(raw).toBeDefined();
 		expect(el).toBeDefined();
 
+
 		expect($).toBeDefined();
 
 		// expect(fixture.el.firstChild).toBe(this.result[0][0]);
@@ -45,149 +47,149 @@ describe("Imagine/component/element", function(){
 	it("should throw an error if passed a non html object", function(){
 		expect(function(){new Element({});}).toThrow(new Error("Not a HTML object"));
 	});
-	//
-	// it("should attach a name of element", function(){
-	// 	var div = document.createElement("DIV");
-	// 	var el = new Imagine.Element(div);
-	// 	expect(el.name).toBe('element');
-	// 	expect(el.tags.indexOf('element')).toBe(0);
-	// });
-	//
-	// describe("constructor", function(){
-	// 	it("should return an element component", function(){
-	// 		var comp = new Imagine.Element(raw);
-	// 		expect(comp.name).toBe("element");
-	// 	});
-	// 	it("should copy some functinos to the element", function(){
-	// 		var div = document.createElement("DIV");
-	// 		new Imagine.Element(div);
-	// 		expect(div.getLocalRect).toBeDefined();
-	// 		expect(div.move).toBeDefined();
-	// 		expect(div.moveTo).toBeDefined();
-	// 	});
-	// });
-	//
-	// describe("raw", function(){
-	// 	it("should be the object passed into the constructor", function(){
-	// 		var comp = new Imagine.Element(raw);
-	// 		expect(comp.raw).toBe(raw);
-	// 	});
-	// });
-	//
-	//
-	// describe("getLocalRect", function(){
-	// 	it("should work with border");
-	// 	it("should work with margin");
-	// 	it("should work with padding");
-	// 	it("should be defined", function(){
-	// 		var div = document.createElement("DIV");
-	// 		var comp = new Imagine.Element(div);
-	// 		expect(comp.getLocalRect).toBeDefined();
-	// 	});
-	// 	it("should return a rect object", function(){
-	// 		// var div = document.createElement("DIV");
-	// 		// var el = Imagine.element(div);
-	// 		var rect = el.getLocalRect();
-	// 		expect(rect.top).toBeDefined();
-	// 		expect(rect.right).toBeDefined();
-	// 		expect(rect.bottom).toBeDefined();
-	// 		expect(rect.left).toBeDefined();
-	// 		expect(rect.height).toBeDefined();
-	// 		expect(rect.width).toBeDefined();
-	// 	});
-	// });
-	//
-	// describe("move", function(){
-	//
-	// 	it('should be precise', function(){
-	//
-	// 		el.move(1,1);//prime it
-	//
-	// 		start = el.getLocalRect().left;
-	//
-	// 		amnt = 1.2345;
-	// 		el.move(amnt, 0);
-	// 		now = el.getLocalRect().left;
-	//
-	// 		expected = now-start;
-	//
-	// 		diff = Math.abs(expected - amnt);
-	//
-	// 		expect(diff).toBeLessThan(0.3);//for phantom, 0.0005 for browsers
-	// 	});
-	//
-	// 	it("shuold handle rounding errors");
-	// 		//css = 1.5px
-	// 		//move(1px)
-	// 		//expect css = 2px;
-	//
-	// 	it("should have move", function(){
-	// 		expect(el.move).toBeDefined();
-	// 	});
-	// 	it("should have moveTo", function(){
-	// 		expect(el.moveTo).toBeDefined();
-	//
-	// 	});
-	//
-	// 	it("should move the right amount", function(){
-	//
-	// 		// console.log(el);
-	// 		t = el.getPosition().top;
-	// 		l = el.getPosition().left;
-	//
-	// 		expect(Imagine.utils.isElement(el.raw)).toBe(true);
-	// 		el.move(1,2);
-	//
-	// 		// expect(el.offsetLeft).toBe("")
-	// 		// expect(el.style.left).toBe("sometihng")
-	//
-	//
-	// 		expect(el.getPosition().top).toBe(t+2);
-	// 		expect(el.getPosition().left).toBe(l+1);
-	//
-	// 		// expect($(el).position().left).toBe(el.rect().left)
-	//
-	// 		// el.move(1,2)
-	// 		// expect(el.rect().top).toBe(t+4)
-	// 		// expect(el.rect().left).toBe(l+2)
-	// 	});
-	//
-	// });
-	//
-	// describe('position', function(){
-	// 	describe('get', function(){
-	// 		it("should be defined", function(){
-	// 			expect(el.getPosition).toBeDefined();
-	// 		});
-	// 		it("should detect itself if not set", function(){
-	// 			otop = el.raw.offsetTop;
-	// 			left = el.raw.offsetLeft;
-	// 			pos = el.getPosition();
-	// 			expect(pos.top).toBe(otop);
-	// 			expect(pos.left).toBe(left);
-	// 		});
-	// 	});
-	// 	describe('set', function(){
-	// 		it("should be defined", function(){
-	// 			expect(el.setPosition).toBeDefined();
-	// 		});
-	// 		it("should return itself for chaining", function(){
-	// 			expect(el.setPosition(1,2)).toBe(el);
-	// 		});
-	//
-	// 		it("should set the positions", function(){
-	// 			el.setPosition(3,4);
-	// 			expect(el.raw.style.left).toBe('3px');
-	// 			expect(el.raw.style.top).toBe('4px');
-	// 		});
-	// 	});
-	//
-	//
-	// 	it("should set and return the stored position", function(){
-	// 		el.setPosition(5,6);
-	// 		pos = el.getPosition();
-	// 		expect(pos.left).toBe(5);
-	// 		expect(pos.top).toBe(6);
-	// 	});
-	// });
+
+	it("should attach a name of element", function(){
+		var div = document.createElement("DIV");
+		var el = new Element(div);
+		expect(el.type).toBe('element');
+		expect(el.tags.indexOf('element')).toBe(0);
+	});
+
+	describe("constructor", function(){
+		it("should return an element component", function(){
+			var comp = new Element(raw);
+			expect(comp.type).toBe("element");
+		});
+		it("should copy some functinos to the element", function(){
+			var div = document.createElement("DIV");
+			new Element(div);
+			expect(div.getLocalRect).toBeDefined();
+			expect(div.move).toBeDefined();
+			expect(div.moveTo).toBeDefined();
+		});
+	});
+
+	describe("raw", function(){
+		it("should be the object passed into the constructor", function(){
+			var comp = new Element(raw);
+			expect(comp.raw).toBe(raw);
+		});
+	});
+
+
+	describe("getLocalRect", function(){
+		it("should work with border");
+		it("should work with margin");
+		it("should work with padding");
+		it("should be defined", function(){
+			var div = document.createElement("DIV");
+			var comp = new Element(div);
+			expect(comp.getLocalRect).toBeDefined();
+		});
+		it("should return a rect object", function(){
+			// var div = document.createElement("DIV");
+			// var el = Imagine.element(div);
+			var rect = el.getLocalRect();
+			expect(rect.top).toBeDefined();
+			expect(rect.right).toBeDefined();
+			expect(rect.bottom).toBeDefined();
+			expect(rect.left).toBeDefined();
+			expect(rect.height).toBeDefined();
+			expect(rect.width).toBeDefined();
+		});
+	});
+
+	describe("move", function(){
+
+		it('should be precise', function(){
+
+			el.move(1,1);//prime it
+
+			start = el.getLocalRect().left;
+
+			amnt = 1.2345;
+			el.move(amnt, 0);
+			now = el.getLocalRect().left;
+
+			expected = now-start;
+
+			diff = Math.abs(expected - amnt);
+
+			expect(diff).toBeLessThan(0.3);//for phantom, 0.0005 for browsers
+		});
+
+		it("shuold handle rounding errors");
+			//css = 1.5px
+			//move(1px)
+			//expect css = 2px;
+
+		it("should have move", function(){
+			expect(el.move).toBeDefined();
+		});
+		it("should have moveTo", function(){
+			expect(el.moveTo).toBeDefined();
+
+		});
+
+		it("should move the right amount", function(){
+
+			// console.log(el);
+			t = el.getPosition().top;
+			l = el.getPosition().left;
+
+			expect(utils.isElement(el.raw)).toBe(true);
+			el.move(1,2);
+
+			// expect(el.offsetLeft).toBe("")
+			// expect(el.style.left).toBe("sometihng")
+
+
+			expect(el.getPosition().top).toBe(t+2);
+			expect(el.getPosition().left).toBe(l+1);
+
+			// expect($(el).position().left).toBe(el.rect().left)
+
+			// el.move(1,2)
+			// expect(el.rect().top).toBe(t+4)
+			// expect(el.rect().left).toBe(l+2)
+		});
+
+	});
+
+	describe('position', function(){
+		describe('get', function(){
+			it("should be defined", function(){
+				expect(el.getPosition).toBeDefined();
+			});
+			it("should detect itself if not set", function(){
+				otop = el.raw.offsetTop;
+				left = el.raw.offsetLeft;
+				pos = el.getPosition();
+				expect(pos.top).toBe(otop);
+				expect(pos.left).toBe(left);
+			});
+		});
+		describe('set', function(){
+			it("should be defined", function(){
+				expect(el.setPosition).toBeDefined();
+			});
+			it("should return itself for chaining", function(){
+				expect(el.setPosition(1,2)).toBe(el);
+			});
+
+			it("should set the positions", function(){
+				el.setPosition(3,4);
+				expect(el.raw.style.left).toBe('3px');
+				expect(el.raw.style.top).toBe('4px');
+			});
+		});
+
+
+		it("should set and return the stored position", function(){
+			el.setPosition(5,6);
+			pos = el.getPosition();
+			expect(pos.left).toBe(5);
+			expect(pos.top).toBe(6);
+		});
+	});
 });
--- src/component/element.js
+++ src/component/element.js
@@ -8,5 +8,47 @@ module.exports = class Element {
     this.type = 'element';
     this.tags = ['element'];
     this.raw = el;
+    this.raw.getLocalRect = this.getLocalRect;
+    this.raw.move = this.move;
+    this.raw.moveTo = this.moveTo;
+  }
+  getLocalRect(){
+    let rect = this.raw.getBoundingClientRect();
+    let parent = this.raw.parentNode;
+    if(parent){
+      parent = parent.getBoundingClientRect();
+      rect = {
+        right: rect.right - parent.right,
+        bottom: rect.bottom - parent.bottom,
+        top: rect.top - parent.top,
+        left: rect.left - parent.left,
+        height: rect.bottom - parent.top,
+        width: rect.right - parent.left,
+      };
+    }
+    return rect;
+  }
+  posInit(){
+    this._pos = {
+      left: this.raw.offsetLeft,
+      top: this.raw.offsetTop
+    }
+  }
+  getPosition(){
+    if(!this._pos) this.posInit();
+    return this._pos;
+  }
+  setPosition(x, y){
+    this._pos = {'left':x, 'top':y};
+    this.raw.style.top = y+"px";
+    this.raw.style.left = x+"px";
+    return this;
+  }
+  moveTo(x, y){
+    return this.setPosition(x, y);
+  }
+  move(x, y){
+    const pos = this.getPosition();
+    this.setPosition(pos.left+x, pos.top+y);
   }
 };
